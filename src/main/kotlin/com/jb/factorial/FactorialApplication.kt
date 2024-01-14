c63dda524e0666ea1a7a8171ff168901bb2c5c05
Jorge Fernández Bautista <jfernandez@idealista.com>
2024-01-14 18:18:21 +0100

    init commit

---
+++ src/main/kotlin/com/jb/factorial/FactorialApplication.kt
@@ -0,0 +1,11 @@
+package com.jb.factorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FactorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<FactorialApplication>(*args)
+}
