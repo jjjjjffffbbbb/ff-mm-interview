22fa8ce9d38fc44bc8c53e4327967bcfb0b10d20
Jorge Fernández Bautista <jfernandez@idealista.com>
2024-01-18 00:19:14 +0100

    fix response of the get metric controller

--- src/main/kotlin/com/jb/factorial/metrics/query/infrastructure/api/GetMetricsController.kt
+++ src/main/kotlin/com/jb/factorial/metrics/query/infrastructure/api/GetMetricsController.kt
@@ -3,10 +3,14 @@ package com.jb.factorial.metrics.query.infrastructure.api
 import com.jb.factorial.metrics.query.application.GetMetrics
 import com.jb.factorial.metrics.query.application.GetMetricsHandler
 import com.jb.factorial.metrics.query.application.GroupBy
+import com.jb.factorial.metrics.query.infrastructure.jpa.MetricProjection
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+data class GetMetricsResponse(val metrics: List<MetricProjection>)
 
 @RestController
 class GetMetricsController(
@@ -14,16 +18,15 @@ class GetMetricsController(
 ) {
 
     @GetMapping(URL)
-    fun getMetrics(@RequestParam groupBy: String): ResponseEntity<*> {
-
+    fun getMetrics(@RequestParam groupBy: String): ResponseEntity<*> =
         GroupBy.parse(groupBy)
-            .map { GetMetrics(it) }
-            .map { handler.handle(it) }
-        return TODO()
-    }
+            .map(::GetMetrics)
+            .map(handler::handle)
+            .map(::GetMetricsResponse)
+            .fold( { throw IllegalStateException("error") }, ::ok)
 
 
     companion object {
-        const val URL = "/metrics"
+        private const val URL = "/metrics"
     }
 }
